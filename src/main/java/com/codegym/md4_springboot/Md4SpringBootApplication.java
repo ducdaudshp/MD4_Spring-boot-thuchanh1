8d4717e7e9276dde458c95e76c979b9c0417d8b3
ducdaudshp <ducdaudshp@gmail.com>
2022-05-05 10:14:47 +0700

    tao va chay ung dung spring boot

---
+++ src/main/java/com/codegym/md4_springboot/Md4SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.md4_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/md4_springboot/Md4SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.md4_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
